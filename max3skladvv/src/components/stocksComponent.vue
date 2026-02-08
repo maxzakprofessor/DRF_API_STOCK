cbe7fea7426ec8f8b4e7bf55a9e5bcae0303c376
Максат Закирьянов <m.zakiryanov70@outlook.com>
2026-02-08 16:38:41 +0500

    Fix URL

--- max3skladvv/src/components/stocksComponent.vue
+++ max3skladvv/src/components/stocksComponent.vue
@@ -151,7 +151,8 @@ export default {
  //        API_URL:"https://localhost:7141/api/",
  //        API_URL:"http://127.0.0.1:8000/",
  //        API_URL:"https://mzakiryanovgmailcom.pythonanywhere.com/"
- API_URL : import.meta.env.VITE_API_URL || "https://mzakiryanovgmailcom.pythonanywhere.com/",        
+ //API_URL : import.meta.env.VITE_API_URL || "https://mzakiryanovgmailcom.pythonanywhere.com/",
+API_URL : import.meta.env.VITE_API_URL || "https://sklad-backend-docker.onrender.com/",       
  //API_URL:"https://webapistockkz.azurewebsites.net/api/",
 
     }
